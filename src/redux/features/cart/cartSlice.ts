9cff56595e64dfaba3810bd999228e17bcfa7626
dev-alamin1 <dev.alaminmondal@gmail.com>
2023-07-11 11:21:51 +0600

    existed cart product check and quantity updated

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -17,7 +17,21 @@ const cartSlice = createSlice({
     initialState,
     reducers:{
         addtoCart :(state,action:PayloadAction<IProduct>)=>{
-            state.products.push(action.payload)
+            //! befor add to cart , we have to check that product is already exits on cart or not
+            const existed = state.products.find(produt=>produt._id === action.payload._id )
+          
+            //!Action theke payload er maddhome asa , product er id jodi cart a existed
+            //!kono product er idr sathe match kore , tahole sudhu matro product er quantity 
+            //! increase kore dibo.. ar match na korle, oi product ke cart er product array te push korbo
+
+            if(existed){
+                existed.quantity = existed.quantity! +1; //! quantity er por what sign nirdes kore, ami sure aci ekhane undefined hobe na
+            }else{
+                state.products.push({...action.payload, quantity:1})
+                //singlgle vabe product add korle, product add hobe and tar sathe quantity 1 set hobe
+            }
+
+             
         }
     }
 
