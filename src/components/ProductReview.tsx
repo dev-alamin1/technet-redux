2ee183a02194efd834802512c19e636b675f65e0
dev-alamin1 <dev.alaminmondal@gmail.com>
2023-07-14 11:14:30 +0600

    get specific product comments

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -1,43 +1,39 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import {  useRef } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
-import { useSetCommentMutation } from '@/redux/api/apiSlice';
+import { useGetCommentQuery, usePostCommentMutation } from '@/redux/api/apiSlice';
 
-const dummyComments = [
-  'Bhalo na',
-  'Ki shob ghori egula??',
-  'Eta kono product holo ??',
-  '200 taka dibo, hobe ??',
-];
 
+// const dummyComments = [
+//   'Bhalo na',
+//   'Ki shob ghori egula??',
+//   'Eta kono product holo ??',
+//   '200 taka dibo, hobe ??',
+// ];
 
 
 export default function ProductReview({ productId }:{productId:string |undefined}) {
-  const [comment, setComment] = useState<string>('');
+   const commentRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setComment(event.target.value);
-  };
+   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+     event.preventDefault();
+     userComment({ id: productId, comment: commentRef.current?.value || '' });
+     commentRef.current!.value = ''; // Reset the form input value
+   };
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    userComment({ id: productId, comment });
-  };
-
-  const [userComment] = useSetCommentMutation();
+  const [userComment] = usePostCommentMutation();
   //! first function (function er nam icche moto dite parbo) , then option ->option is and object so we can destructure
   // ! as like as const [setComment, {isLoading,error}] = useSetCommentMutation();
+
+
+  const { data } = useGetCommentQuery(productId);
 
   return (
     <div className="max-w-7xl mx-auto mt-5">
       <form className="flex gap-5 items-center" onSubmit={handleSubmit}>
-        <Textarea
-          className="min-h-[30px]"
-          name="comment"
-          onChange={handleChange}
-        />
+        <Textarea className="min-h-[30px]" name="comment" ref={commentRef} />
         <Button
           type="submit"
           className="rounded-full h-10 w-10 p-2 text-[25px]"
@@ -47,7 +43,7 @@ export default function ProductReview({ productId }:{productId:string |undefined
       </form>
 
       <div className="mt-10">
-        {dummyComments.map((comment, index) => (
+        {data?.comments?.map((comment:string, index:number) => (
           <div key={index} className="flex gap-3 items-center mb-5">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
